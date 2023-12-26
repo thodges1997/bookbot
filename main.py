0d1627575741f1aa893163cd968201748d2233d4
Timothy Hodges <timh@Timothys-MacBook-Pro.local>
2023-12-26 12:45:43 -0500

    update to bookbot

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
-with open("books/franky.txt") as f:
-    book = f.read()
-    print(book)
+def main():
+    book = get_book_text("books/franky.txt")
+    words = get_num_words(book)
+    print(words)
+
+def get_num_words(text):
+    words = text.split()
+    return len(words)
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+
+main()
